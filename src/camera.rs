56b8b00df04e51803c235b8bae09f138b58a30f1
Aleš Matěj <amatej@redhat.com>
2022-09-17 20:52:49 +0200

    Limit the camera movement to world bounds

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,7 @@
-
 use bevy::prelude::*;
+
 use crate::player;
+use crate::config;
 
 pub fn camera_follow_player(
     mut player_query: Query<&Transform, With<player::Player>>,
@@ -8,6 +9,22 @@ pub fn camera_follow_player(
 ) {
     let player_ship_transform = player_query.single_mut();
     let mut camera_transform = camera_query.single_mut();
-    camera_transform.translation.x = player_ship_transform.translation.x;
-    camera_transform.translation.y = player_ship_transform.translation.y;
+
+    let mut new_camera_pos = player_ship_transform.translation.truncate();
+    if new_camera_pos.x > config::MAP_BOUNDS.x/2.0 - config::WINDOW_BOUNDS.x/2.0 {
+        new_camera_pos.x = config::MAP_BOUNDS.x/2.0 - config::WINDOW_BOUNDS.x/2.0;
+    }
+    if new_camera_pos.x < -config::MAP_BOUNDS.x/2.0 + config::WINDOW_BOUNDS.x/2.0 {
+        new_camera_pos.x = -config::MAP_BOUNDS.x/2.0 + config::WINDOW_BOUNDS.x/2.0;
+    }
+
+    if new_camera_pos.y > config::MAP_BOUNDS.y/2.0 - config::WINDOW_BOUNDS.y/2.0 {
+        new_camera_pos.y = config::MAP_BOUNDS.y/2.0 - config::WINDOW_BOUNDS.y/2.0;
+    }
+    if new_camera_pos.y < -config::MAP_BOUNDS.y/2.0 + config::WINDOW_BOUNDS.y/2.0 {
+        new_camera_pos.y = -config::MAP_BOUNDS.y/2.0 + config::WINDOW_BOUNDS.y/2.0;
+    }
+
+    camera_transform.translation.x = new_camera_pos.x;
+    camera_transform.translation.y = new_camera_pos.y;
 }
